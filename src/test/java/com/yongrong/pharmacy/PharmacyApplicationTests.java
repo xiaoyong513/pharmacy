78fa82c6a23e379e67d43b78b9cb367ab0ee7ee0
xiaoma513 <1104672014@qq.com>
2019-01-23 17:37:31 +0800

    Initial commit

---
+++ src/test/java/com/yongrong/pharmacy/PharmacyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yongrong.pharmacy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PharmacyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
